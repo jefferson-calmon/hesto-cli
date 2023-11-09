99f28f45c7395a46fefb6f332da57af6e4370d85
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-11-09 13:23:18 -0300

    :hammer: refactor(contents/model): Improve the base model content

--- src/contents/model.ts
+++ src/contents/model.ts
@@ -1,14 +1,25 @@
 /**
  * @param %name%
  */
-export const model = `import { BaseModel } from './Base';
+export const model = `import { Validations } from 'codekit';
+
+import { BaseModel } from './Base';
 
 export interface %name% extends BaseModel {
+
+}
+
+export const initial%name%: %name% = {
+
+}
+
+export const validations: Validations<%name%> = {
 
 }
 `;
 
 export const modelWithZod = `import { z } from 'zod';
+import { Validations } from 'codekit';
 
 import { BaseModelSchema, WithoutBaseProps } from './Base';
 
@@ -20,4 +31,12 @@ export const %name%Schema = BaseModelSchema.merge(
 
 export type %name% = z.infer<typeof %name%Schema>;
 export type %name%Mutation = WithoutBaseProps<z.infer<typeof %name%Schema>>;
+
+export const initial%name%: %name% = {
+
+}
+
+export const validations: Validations<%name%> = {
+
+}
 `;
